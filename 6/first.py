2268da9956906eb61d97eea1d432171e126b6f88
Roman Garanin <git@romangaranin.net>
2016-04-16 10:56:53 +0400

    Commands factory concept

--- 6/first.py
+++ 6/first.py
@@ -1,5 +1,7 @@
 import unittest
 
+
+# commands
 class Command:
     TURN_OFF = "turn off"
     TURN_ON = "turn on"
@@ -9,31 +11,56 @@ class Command:
     from_coord = ()
     to_coord = ()
 
-    # turn on 489,959 through 759,964
-    def __init__(self, command_string):
-        self.type = Command.get_command_type(command_string)
-        self.from_coord, self.to_coord = Command.get_coords(command_string)
+    def __init__(self, from_coord, to_coord):
+        super().__init__()
+        self.from_coord = from_coord
+        self.to_coord = to_coord
 
+    def execute(self, lights_map):
+        pass
+
+
+class TurnOffCommand(Command):
+    pass
+
+
+class TurnOnCommand(Command):
+    pass
+
+
+class ToggleCommand(Command):
+    pass
+# commands ended
+
+
+class CommandFactory:
+    # turn on 489,959 through 759,964
     @classmethod
-    def get_command_type(cls, command_string):
-        if command_string.startswith(cls.TURN_OFF):
-            return cls.TURN_OFF
-        elif command_string.startswith(cls.TURN_ON):
-            return cls.TURN_ON
-        elif command_string.startswith(cls.TOGGLE):
-            return cls.TOGGLE
+    def get_command(cls, command_string):
+        from_coord, to_coord = cls.get_coords(command_string)
+
+        return cls.create_command(command_string, from_coord, to_coord)
+
+    @staticmethod
+    def create_command(command_string, from_coord, to_coord):
+        if command_string.startswith(Command.TURN_OFF):
+            return TurnOffCommand(from_coord, to_coord)
+        elif command_string.startswith(Command.TURN_ON):
+            return TurnOnCommand(from_coord, to_coord)
+        elif command_string.startswith(Command.TOGGLE):
+            return ToggleCommand(from_coord, to_coord)
         else:
             raise ValueError("Command '{0}' - command type cannot be recognised".format(command_string))
 
     @classmethod
     def get_coords(cls, command_string):
+        """
+        calculates coordinates from input command string, returns tuple of coordinates - (from, to)
+        """
         command_parts = command_string.split(' ')
 
         to_coords = cls.parse_coords(command_parts[-1])
         from_coords = cls.parse_coords(command_parts[-3])
-
-        #print("to_coords:" + str(to_coords))
-        #print("from_coords:" + str(from_coords))
 
         return tuple(from_coords), tuple(to_coords)
 
@@ -52,17 +79,18 @@ class Command:
         """
         return tuple(map(int, coords_strings_tuple))
 
-
-#array = [[0] * 1000] * 1000
+################################################################################
+# main logic
+# array = [[0] * 1000] * 1000
 
 
 ###############################################################################
 # Tests
 class TestCommand(unittest.TestCase):
     def test_command_type(self):
-        command = Command("turn on 489,959 through 759,964")
+        command = CommandFactory.get_command("turn on 489,959 through 759,964")
 
-        self.assertEqual(command.type, Command.TURN_ON)
+        self.assertTrue(isinstance(command, TurnOnCommand))
         self.assertEqual(command.from_coord, (489, 959))
         self.assertEqual(command.to_coord, (759, 964))
 
