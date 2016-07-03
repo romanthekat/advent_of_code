d852df62940d974719f2bc0aa3be0b382a407fc5
Roman Garanin <git@romangaranin.net>
2016-07-03 10:49:46 +0400

    Basic circuit class

--- 7/first.py
+++ 7/first.py
@@ -1,8 +1,25 @@
-import AdventOfCodeHelper
+from AdventOfCodeHelper import get_input_lines
 
 
-class Schema:
+class Circuit:
     wires = {}
 
+    def read_schema(self):
+        for line in get_input_lines():
+            self._parse_line(line)
 
-print(AdventOfCodeHelper.get_input_lines())
+    def _parse_line(self, line):
+        if "AND" in line:
+            pass
+        elif "OR" in line:
+            pass
+        elif "NOT" in line:
+            pass
+        elif "LSHIFT" in line:
+            pass
+        elif "RSHIFT" in line:
+            pass
+        else:  # raw value to the gate situation
+
+
+circuit = Circuit()
