5fc0566ccaa771875a0cd303c67c10384d2e3c09
Roman Garanin <git@romangaranin.net>
2017-12-01 23:51:47 +0400

    2017 task 1 - second half

--- 2017/task_1/main.go
+++ 2017/task_1/main.go
@@ -11,16 +11,19 @@ import (
 
 func main() {
 	input := readInput()
-	result := solve(input)
 
-	fmt.Println(result)
+	resultFirst := solveFirst(input)
+	fmt.Println(resultFirst)
+
+	resultSecond := solveSecond(input)
+	fmt.Println(resultSecond)
 }
 
-func solve(input string) int {
+func solveFirst(input string) int {
 	sum := 0
 	lastNum := getLastNum(input)
 	for _, c := range input {
-		currNum := getNumByChar(string(c))
+		currNum := getNumByString(string(c))
 
 		if lastNum == currNum {
 			sum += currNum
@@ -32,12 +35,39 @@ func solve(input string) int {
 	return sum
 }
 
+func solveSecond(input string) int {
+	sum := 0
+	inputSize := utf8.RuneCountInString(input)
+	jumpSize := inputSize/2
+
+	for index, c := range input {
+		currNum := getNumByString(string(c))
+		nextNum := getNextNum(input, index, inputSize, jumpSize)
+
+		if currNum == nextNum {
+			sum += currNum
+		}
+	}
+
+	return sum
+}
+
+func getNextNum(input string, index int, inputSize int, jumpSize int) int {
+	resultIndex := index + jumpSize
+	if resultIndex >= inputSize {
+		resultIndex = resultIndex % inputSize
+	}
+
+	return getNumByString(string(input[resultIndex]))
+}
+
 func getLastNum(input string) int {
 	lastChar := string(input[utf8.RuneCountInString(input) - 1])
 
-	return getNumByChar(lastChar)
+	return getNumByString(lastChar)
 }
-func getNumByChar(lastChar string) int {
+
+func getNumByString(lastChar string) int {
 	lastNum, err := strconv.Atoi(lastChar)
 	if err != nil {
 		panic("Cannot get last num:" + err.Error())
