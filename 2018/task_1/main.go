6d49bf130cdb20d8373f5d3b442f0fb0fed7b7f9
Roman Garanin <git@romangaranin.net>
2018-12-01 12:02:40 +0300

    2018 task 1 - first part

---
+++ 2018/task_1/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+func main() {
+	input := readInput()
+
+	resultFirst := solveFirst(input)
+	fmt.Println(resultFirst)
+
+	//resultSecond := solveSecond(input)
+	//fmt.Println(resultSecond)
+}
+
+func solveFirst(input[] string) int {
+	freq := 0
+
+	for _, inputLine := range input {
+		freq += getNumByString(inputLine)
+	}
+
+	return freq
+}
+
+
+func getNumByString(numRaw string) int {
+	num, err := strconv.Atoi(numRaw)
+	if err != nil {
+		panic("Cannot get num:" + err.Error())
+	}
+	return num
+}
+
+func readInput() []string {
+	file, err := os.Open("./input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer file.Close()
+
+	var result []string
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		result = append(result, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return result
+}
