77b3bff66461b7676c06e7020c82b11c33763269
Roman Garanin <git@romangaranin.net>
2018-12-02 21:40:34 +0300

    2018 task 1 - tests

---
+++ 2018/task_1/main_test.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestFirst(t *testing.T) {
+	input := []string {
+		"1",
+		"2",
+		"-3",
+		"42",
+	}
+	result := solveFirst(input)
+
+	checkResult(t, result, 42)
+}
+
+func TestSecond(t *testing.T) {
+	input := []string {
+		"1",
+		"2",
+		"-3",
+		"2",
+	}
+	result := solveSecond(input)
+
+	checkResult(t, result, 3)
+}
+
+func checkResult(t *testing.T, actualResult int, requiredResult int) {
+	t.Helper()
+
+	if actualResult != requiredResult {
+		t.Error(fmt.Printf("checksum must be %+v, but: %+v", requiredResult, actualResult))
+	}
+}
