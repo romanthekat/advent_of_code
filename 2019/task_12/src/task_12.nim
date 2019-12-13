1e4d3c3c294d2dc89cb7ffedb112f6a5a8da79b7
Roman Garanin <git@romangaranin.net>
2019-12-13 23:15:12 +0300

    2019 task 2: part 2

--- 2019/task_12/src/task_12.nim
+++ 2019/task_12/src/task_12.nim
@@ -1,12 +1,12 @@
 import math, strutils, strformat, sequtils
 
 type 
-  Moon* = ref object
-    x*, y*, z*: int
-    vX*, vY*, vZ*: int
+  Moon* = ref tuple 
+    x, y, z: int
+    vX, vY, vZ: int
 
 proc solveFirst*(input: seq[string], steps: int): int
-proc solveSecond*(input: seq[string]): int
+proc solveSecond*(input: seq[string]): uint64
 
 
 when isMainModule:
@@ -18,10 +18,21 @@ when isMainModule:
 proc getCoor(line: string): int =
   return line.split('=')[1].parseInt
 
+proc newMoon*(x, y, z, vX, vY, vZ: int): Moon =
+  var moon: Moon
+  new(moon)
+  moon.x = x
+  moon.y = y
+  moon.z = z
+  moon.vX = vX
+  moon.vY = vY
+  moon.vZ = vZ
+  return moon 
+
 proc getMoon(line: string): Moon =
   let parts = line[1..^2].split(',')
 
-  return Moon(x: getCoor(parts[0]), y: getCoor(parts[1]), z: getCoor(parts[2]), vX: 0, vY: 0, vZ: 0)
+  return newMoon(getCoor(parts[0]), getCoor(parts[1]), getCoor(parts[2]), 0, 0, 0)
 
 proc getMoons(input: seq[string]): seq[Moon] =
   return input.mapIt(getMoon(it))
@@ -61,23 +72,82 @@ proc calculateEnergy*(moons: seq[Moon]): int =
           .mapIt(it.calculateEnergy)
           .foldl(a + b)
 
+proc performStep(moons: seq[Moon]) =
+  for i in 0..<moons.len:
+    for j in i+1..<moons.len:
+      var first = moons[i]
+      var second = moons[j]
+      applyGravity(first, second)
+  
+  for moon in moons:
+    var refMoon = moon
+    refMoon.applyVelocity()
+
+
+proc same(originalMoons, moons: seq[Moon], checkX = false, checkY = false, checkZ = false): bool =
+  let pairs = zip(originalMoons, moons)
+  for pair in pairs:
+    let first = pair[0]
+    let second = pair[1]
+
+    if checkX: 
+      if first.x != second.x or first.vX != second.vX: return false
+
+    if checkY: 
+      if first.y != second.y or first.vY != second.vY: return false
+    
+    if checkZ: 
+      if first.z != second.z or first.vZ != second.vZ: return false
+
+  return true 
+
+proc getLcm(x, y, z: uint64): uint64 =
+  var mult: uint64 = 1
+
+  while true:
+    mult += 1
+
+    let lcm = x * mult
+
+    if (lcm mod y) == 0 and (lcm div y) >= 1.uint64 and (lcm mod z) == 0 and (lcm div z) >= 1.uint64:
+      return lcm
+    
+    if mult mod 100000000 == 0:
+      echo fmt"mult:{mult}"
+
 proc solveFirst*(input: seq[string], steps: int): int =
   let moons = getMoons(input)
 
   for step in 1..steps:
-    for i in 0..<moons.len:
-      for j in i+1..<moons.len:
-        var first = moons[i]
-        var second = moons[j]
-        applyGravity(first, second)
-    
-    for moon in moons:
-      var refMoon = moon
-      refMoon.applyVelocity()
+    moons.performStep()
 
   return moons.calculateEnergy()
   
-
-proc solveSecond*(input: seq[string]): int =
+proc solveSecond*(input: seq[string]): uint64 =
+  let originalMoons = getMoons(input)
   let moons = getMoons(input)
-  return -1
+
+  var sameX = false
+  var sameY = false
+  var sameZ = false 
+  var cycleX: uint64 = 0
+  var cycleY: uint64 = 0
+  var cycleZ: uint64 = 0
+
+  var steps: uint64 = 0
+  while not (sameX and sameY and sameZ):
+    steps += 1
+    #TODO perform calculation to only required axis
+    moons.performStep()
+
+    if not sameX:
+      sameX = same(originalMoons, moons, checkX=true)
+      if sameX: cycleX = steps
+    if not sameY:
+      sameY = same(originalMoons, moons, checkY=true)
+      if sameY: cycleY = steps
+    if not sameZ:
+      sameZ = same(originalMoons, moons, checkZ=true)
+      if sameZ: cycleZ = steps
+
+  return getLcm(cycleX, cycleY, cycleZ) 
--- 2019/task_12/tests/ttask_12.nim
+++ 2019/task_12/tests/ttask_12.nim
@@ -2,8 +2,17 @@ import ../src/task_12
 import strutils, strformat
 
 when isMainModule:
-  doAssert Moon(x: 8, y: 12, z: -9, vX: -7, vY: 3, vZ: 0).calculateEnergy() == 290
+  doAssert newMoon(8, 12, -9, -7, 3, 0).calculateEnergy() == 290
 
-  let input = @["<x=-1, y=0, z=2>", "<x=2, y=-10, z=-7>", "<x=4, y=-8, z=8>", "<x=3, y=5, z=-1>"]
-  doAssert solveFirst(input, 10) == 179
+  let input1 = @["<x=-1, y=0, z=2>", "<x=2, y=-10, z=-7>", "<x=4, y=-8, z=8>", "<x=3, y=5, z=-1>"]
+  doAssert solveFirst(input1, 10) == 179
   
+  let input2 = @["<x=-1, y=0, z=2>", "<x=2, y=-10, z=-7>", "<x=4, y=-8, z=8>", "<x=3, y=5, z=-1>"]
+  let result2 = solveSecond(input2)
+  echo result2
+  doAssert result2 == 2772.uint64
+
+  let longInput = @["<x=-8, y=-10, z=0>", "<x=5, y=5, z=10>", "<x=2, y=-7, z=3>", "<x=9, y=-8, z=-3>"] 
+  let resultLongInput = solveSecond(longInput)
+  echo resultLongInput
+  doAssert resultLongInput == 4686774924.uint64
