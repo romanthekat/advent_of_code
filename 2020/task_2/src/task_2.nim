126f2fa9d7cb325781567fb824ee6b086ab12005
Roman Garanin <git@romangaranin.net>
2020-12-02 21:01:04 +0300

    2020 task 2: part 2

--- 2020/task_2/src/task_2.nim
+++ 2020/task_2/src/task_2.nim
@@ -25,10 +25,31 @@ proc solveFirst*(input: seq[string]): int =
 
     return result
 
+proc solveSecond*(input: seq[string]): int =
+    var result = 0
+
+    #1-3 a: abcde
+    for line in input:
+        let parts = line.split(" ")
+        let countFormat = parts[0].split("-")
+
+        let idx1 = countFormat[0].parseInt
+        let idx2 = countFormat[1].parseInt
+
+        let letter = parts[1][0]
+
+        let password = parts[2]
+
+        if (password[idx1-1] == letter) xor (password[idx2-1] == letter):
+            result += 1
+
+    return result
+
 when isMainModule:
   let input = readFile("input.txt").strip.splitLines
 
-  echo solveFirst(input)
+  echo solveFirst input
+  echo solveSecond input
 
 
 
