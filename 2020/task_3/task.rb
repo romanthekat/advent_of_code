4a1e9927b4c024e7632019f6a86da84a08d2f12e
Roman Garanin <git@romangaranin.net>
2020-12-03 22:40:44 +0300

    2020 task 3: part 2

--- 2020/task_3/task.rb
+++ 2020/task_3/task.rb
@@ -1,18 +1,22 @@
-def solve_first
+def read_input
   area = []
   File.open('input.txt', 'r').each_line do |line|
     area.push line
   end
 
+  area
+end
+
+def iterate(area, dx, dy)
   trees = 0
   x = 0
   y = 0
   loop do
-    y += 1
-    break if area.length == y
+    y += dy
+    break if y >= area.length
 
     line = area[y]
-    x = (x + 3) % (line.length - 1)
+    x = (x + dx) % (line.length - 1)
 
     trees = trees.next if line[x] == '#'
   end
@@ -20,4 +24,13 @@ def solve_first
   trees
 end
 
-puts solve_first
+def solve_first(area)
+  iterate(area, 3, 1)
+end
+
+def solve_second(area)
+  iterate(area, 1, 1) * iterate(area, 3, 1) * iterate(area, 5, 1) * iterate(area, 7, 1) * iterate(area, 1, 2)
+end
+
+puts solve_first read_input
+puts solve_second read_input
