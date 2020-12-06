783e6aa99847365929b898acc50c3202cd3dddfa
Roman Garanin <git@romangaranin.net>
2020-12-06 10:58:48 +0300

    2020 task 5: part 2

--- 2020/task_6/task.rb
+++ 2020/task_6/task.rb
@@ -19,7 +19,30 @@ def solve_first
   total_questions + questions.length
 end
 
+def solve_second
+  questions = {}
+  group_size = 0
+
+  total_questions = 0
+  File.open('input.txt', 'r').each_line do |line|
+    line = line.strip
+    if line.empty?
+      total_questions += questions.filter { |_question, count| count == group_size }.length
+
+      questions.clear
+      group_size = 0
+    else
+      group_size += 1
+      line.each_char do |char|
+        questions[char] = questions.fetch(char, 0) + 1
+      end
+    end
+  end
+  total_questions + questions.filter { |_question, count| count == group_size }.length
+end
+
 
 if $PROGRAM_NAME == __FILE__
   puts solve_first
+  puts solve_second
 end
