9dcc03a81a00fedd653debd3ea0913a744d9a2d0
Roman Garanin <git@romangaranin.net>
2020-12-07 23:21:51 +0300

    2020 task 7: part 2

--- 2020/task_7/task.py
+++ 2020/task_7/task.py
@@ -1,19 +1,44 @@
-class Bag:
+def solve_first(input: list[str]):
+    gold_bag = get_gold_bag(input)
 
+    bags_to_check = [gold_bag]
+    checked_bags = set()
+    while len(bags_to_check) > 0:
+        bag = bags_to_check.pop()
+        checked_bags.add(bag.name)
+
+        bags_to_check += bag.contained
+
+    return len(checked_bags) - 1
+
+
+def solve_second(input):
+    gold_bag = get_gold_bag(input)
+
+    return get_contains_count(gold_bag) - 1
+
+
+class Bag:
     def __init__(self, name: str) -> None:
         super().__init__()
         self.name = name
         self.contains = set()
         self.contained = set()
 
-    def add_contains(self, bag):
-        self.contains.add(bag)
+    def add_contains(self, bag, count: int):
+        self.contains.add(ContainedBag(bag, count))
 
     def add_contained(self, bag):
         self.contained.add(bag)
 
     def __str__(self) -> str:
         return self.name
+
+
+class ContainedBag:
+    def __init__(self, bag, count: int) -> None:
+        self.bag = bag
+        self.count = count
 
 
 def get_bag(bags, bag_name):
@@ -25,47 +50,46 @@ def get_bag(bags, bag_name):
         return bag
 
 
-def clean_bag_name(bag_name, clean_number=True):
+def clean_bag_name(bag_name):
     bag_name = bag_name.strip()
-    if clean_number:
-        bag_name = bag_name[bag_name.index(" ") + 1:]
     bag_name = bag_name[:bag_name.rfind("bag")].strip()
     bag_name = bag_name.replace(".", "")
 
     return bag_name
 
 
-def solve_first(input: list[str]):
+def get_gold_bag(input):
     bags = {}
     for line in input:
         rule = line.split(" contain ")
-        bag = get_bag(bags, clean_bag_name(rule[0], False))
-        contains = rule[1]
+        bag = get_bag(bags, clean_bag_name(rule[0]))
+        contains = rule[1].strip()
 
-        if contains == "no other bags":
+        if contains == "no other bags.":
             pass
         else:
             contained = contains.split(",")
             for contained_bag_name in contained:
                 contained_bag_name = clean_bag_name(contained_bag_name)
+                number_delimiter_index = contained_bag_name.index(" ")
+
+                count = int(contained_bag_name[:number_delimiter_index])
+                contained_bag_name = contained_bag_name[number_delimiter_index + 1:]
+
                 contained_bag = get_bag(bags, contained_bag_name)
 
-                bag.add_contains(contained_bag)
+                bag.add_contains(contained_bag, count)
                 contained_bag.add_contained(bag)
-
     gold_bag = bags["shiny gold"]
+    return gold_bag
 
-    bags_to_check = [gold_bag]
-    checked_bags = set()
-    while len(bags_to_check) > 0:
-        bag = bags_to_check.pop()
-        checked_bags.add(bag.name)
 
-        for contained_bag in bag.contained:
-            bags_to_check.append(contained_bag)
+def get_contains_count(bag: Bag) -> int:
+    count = 1
+    for contained in bag.contains:
+        count += contained.count * get_contains_count(contained.bag)
 
-    return len(checked_bags) - 1
-
+    return count
 
 
 if __name__ == '__main__':
@@ -75,3 +99,4 @@ if __name__ == '__main__':
             input.append(line)
 
     print(solve_first(input))
+    print(solve_second(input))
