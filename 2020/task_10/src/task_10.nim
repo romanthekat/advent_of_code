381136bdc5a59334b100ebd04bf71d6742722849
Roman Garanin <git@romangaranin.net>
2020-12-10 19:06:12 +0300

    docs: comments on task 10 part 2

--- 2020/task_10/src/task_10.nim
+++ 2020/task_10/src/task_10.nim
@@ -24,11 +24,35 @@ proc solveFirst(input: seq[string]): int =
             echo fmt"impossible to choose adapter, currentJolts={currentJolts}, number={number}"
 
     diff3 += 1
-    echo fmt"diff1={diff1}, diff2={diff2}, diff3={diff3}"
 
     return diff1 * diff3
+
+proc calc(index: int, numbers: seq[int]): int =
+    if index == numbers.len() - 1:
+        return 1
+
+    var combinations = 0
+
+    var check_index = index + 1;
+    while check_index < numbers.len() - 1:
+        if numbers[check_index] - numbers[index] <= 3:
+            check_index += 1
+        else:
+            break
+
+    echo fmt"{numbers[index]} {numbers[check_index]} {check_index - index - 1}"
+    return (check_index - index - 1) * calc(check_index, numbers)
+
+#TODO rely on dynamic programming/matrix for calculation - paths count in *graph*
+proc solveSecond(input: seq[string]): int =
+    var numbers: seq[int] = input.map(parseInt)
+    numbers.sort()
+
+    echo numbers
+
+    return calc(0, numbers)
 
 when isMainModule:
     let input = readFile("input.txt").strip.splitLines
     echo solveFirst input
-
+    echo solveSecond input
