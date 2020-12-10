c4f7e1ac297a1fe24f2cffe7a7a3390a88ef5a61
Roman Garanin <git@romangaranin.net>
2020-12-10 12:05:09 +0300

    2020 task 9: part 2

--- 2020/task_9/task.py
+++ 2020/task_9/task.py
@@ -26,6 +26,39 @@ def solve_first(input, preamble_size: int):
     return -1
 
 
+def get_contiguous_set(numbers, invalid_number):
+    for index_1, number_1 in enumerate(numbers):
+        bad_set = [number_1]
+        current_sum = number_1
+        for index_2 in range(index_1 + 1, len(numbers)-1):
+            number_2 = numbers[index_2]
+
+            bad_set.append(number_2)
+            current_sum += number_2
+            if current_sum > invalid_number:
+                bad_set = []
+                break
+            elif current_sum == invalid_number and len(bad_set) > 1:
+                return bad_set
+
+    return []
+
+
+def solve_second(input, invalid_number):
+    numbers = []
+    for line in input:
+        numbers.append(int(line))
+
+    bad_set = get_contiguous_set(numbers, invalid_number)
+    bad_set = sorted(bad_set)
+
+    if len(bad_set) > 2:
+        return bad_set[0] + bad_set[-1]
+
+    print("contiguous set not found")
+    return -1
+
+
 if __name__ == '__main__':
     input = []
     with open("input.txt") as file:
@@ -33,3 +66,4 @@ if __name__ == '__main__':
             input.append(line)
 
     print(f"first: {solve_first(input, 25)}")
+    print(f"second: {solve_second(input, solve_first(input, 25))}")
