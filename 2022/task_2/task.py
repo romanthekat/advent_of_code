d1c1143b5a42656dda42489bf0351d7869a83c65
Roman Garanin <git@romangaranin.net>
2022-12-02 21:47:04 +0100

    2022 task 2: part 2

--- 2022/task_2/task.py
+++ 2022/task_2/task.py
@@ -20,7 +20,27 @@ def solve_first(input: list[str])-> int:
     return result
 
 def solve_second(input: list[str])-> int:
-    return 0
+    result = 0
+    play2score = {"A": 1, "B": 2, "C": 3}
+    play2lose = {"B": "A", "C": "B", "A": "C"}
+    play2win = {"A": "B", "B": "C", "C": "A"}
+    
+    for line in input:
+        opp = line[0]
+        plan = line[2]
+        
+        if plan == 'X':
+            me = play2lose[opp]
+        elif plan == 'Y':
+            me = opp
+            result += 3
+        else:
+            me = play2win[opp]
+            result += 6
+        
+        result += play2score[me]
+        
+    return result
 
 
 if __name__ == '__main__':
