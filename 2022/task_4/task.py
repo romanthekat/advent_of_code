db9a68370c2e7acc34a68f16fb92d5056094f5a8
Roman Garanin <git@romangaranin.net>
2022-12-04 12:52:42 +0100

    2022 task 4: part 1

---
+++ 2022/task_4/task.py
@@ -0,0 +1,33 @@
+def solve_first(input: list[str]) -> int:
+    result = 0
+
+    for line in input:
+        line = line.rstrip()
+        
+        parts = line.split(",")
+        
+        pair1 =[int(n) for n in parts[0].split("-")]
+        pair2 = [int(n) for n in parts[1].split("-")]
+        
+        if (pair1[0] >= pair2[0] and pair1[1] <= pair2[1]) or (pair2[0] >= pair1[0] and pair2[1] <= pair1[1]):
+            result += 1
+
+    return result
+
+
+def solve_second(input: list[str]) -> int:
+    result = 0
+
+    return result
+
+
+if __name__ == "__main__":
+    input = []
+    input_file = "input.txt"
+    # input_file = "input_test.txt"
+    
+    with open(input_file, "r") as f:
+        input = f.readlines()
+
+    print(f"first: {solve_first(input)}")
+    print(f"second: {solve_second(input)}")
