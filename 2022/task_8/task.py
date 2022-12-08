d480394271efb56196dcc72a9cef60b7577aa1c5
Roman Garanin <git@romangaranin.net>
2022-12-08 22:18:44 +0100

    2022 task 8: use black code formatter

--- 2022/task_8/task.py
+++ 2022/task_8/task.py
@@ -1,74 +1,71 @@
 def parse_input(input: list[str]) -> list[list[int]]:
     result = []
-    
+
     for line in input:
         line = line.rstrip()
-        
+
         parsed_line = []
         for tree in line:
             parsed_line.append(int(tree))
-        
+
         result.append(parsed_line)
-    
+
     return result
 
 
-def is_visible(grid: list[list[int]], row, col:int) -> bool:
+def is_visible(grid: list[list[int]], row, col: int) -> bool:
     height = grid[row][col]
-  
-    if row == 0 or row == len(grid)-1 or col == 0 or col == len(grid[0])-1:
+
+    if row == 0 or row == len(grid) - 1 or col == 0 or col == len(grid[0]) - 1:
         return True
-    
-    for check_row in range(row-1, -1, -1):
+
+    for check_row in range(row - 1, -1, -1):
         if grid[check_row][col] >= height:
-           break 
-        
+            break
+
         if check_row == 0:
             return True
-    
-    for check_row in range(row+1, len(grid)):
+
+    for check_row in range(row + 1, len(grid)):
         if grid[check_row][col] >= height:
-           break 
-        
+            break
+
         if check_row == len(grid) - 1:
-            return True    
-    
-    for check_col in range(col-1, -1, -1):
+            return True
+
+    for check_col in range(col - 1, -1, -1):
         if grid[row][check_col] >= height:
-           break 
-        
+            break
+
         if check_col == 0:
             return True
-    
-    for check_col in range(col+1, len(grid[0])):
-        if grid[row][check_col] >= height:
-           break 
-        
-        if check_col == len(grid[0]) - 1:
-            return True  
 
-    return False       
-   
+    for check_col in range(col + 1, len(grid[0])):
+        if grid[row][check_col] >= height:
+            break
+
+        if check_col == len(grid[0]) - 1:
+            return True
+
+    return False
 
 
 def solve_first(input: list[str]) -> int:
     grid = parse_input(input)
-    
+
     result = 0
     for row in range(len(grid)):
         for col in range(len(grid[0])):
             if is_visible(grid, row, col):
                 result += 1
-    
+
     return result
-            
 
 
 def solve_second(input: list[str]) -> int:
-    return -1 
+    return -1
 
 
-    
 # 2945 too high
 if __name__ == "__main__":
     input_file = "input.txt"
